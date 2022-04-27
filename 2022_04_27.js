d23ec9412e90639a141a7b0e6e8ca3a214a73c51
mannMae <daga4242@gmail.com>
2022-04-27 22:40:02 +0900

    22_04_27_01

---
+++ 2022_04_27.js
@@ -0,0 +1,41 @@
+//Q.1
+var nums = [100, 200, 300, 400, 500];
+
+nums.pop();
+nums.pop();
+
+console.log(nums);
+
+//======================================================
+
+//Q.2
+var arr = [200, 100, 300];
+arr.splice(2, 0, 1000);
+console.log(arr);
+
+//===================================================
+
+//Q.3
+var arr = [100, 200, 300];
+console.log(typeof arr); // object(Array)
+
+//====================================================
+
+//Q.4
+
+var a;
+a = 1;
+console.log(typeof a); // number
+
+a = 2.22;
+console.log(typeof a); // number
+
+a = "p";
+console.log(typeof a); // string
+
+a = [1, 2, 3];
+console.log(typeof a); // object
+
+//===================================================
+
+//Q.5
